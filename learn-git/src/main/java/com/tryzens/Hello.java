1b7b04c35330a78ed107e5ce194a9db217185fcf
tryzens <rashmi_kandakur@yahoo.co.in>
2015-01-13 16:05:31 +0530

    c5 master

--- learn-git/src/main/java/com/tryzens/Hello.java
+++ learn-git/src/main/java/com/tryzens/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 		System.out.println("Commit - branch -1");
 	System.out.println("Commit c4 branch 2");
 		System.out.println("Commit c3 branch -1");
+		System.out.println("commit c5");
 
 	}
 
